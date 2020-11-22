cc09d544a0fd3befe8b74f15a176782a03dd9b8f
RXcat1 <73223582+RXcat1@users.noreply.github.com>
2020-11-22 18:37:46 +0200

    Create bot.py

---
+++ bot.py
@@ -0,0 +1,10 @@
+You can just copy and past this code to your discord bot project.
+NOTE: this code only makes your bot online.
+Code:
+
+
+import discord
+
+client = discord.Client
+
+client.run('Your Bot Token')
